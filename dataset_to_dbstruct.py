13059d1f73a8fe29688fde8a3ffc3fb781a18822
Tobias Fischer <info@tobiasfischer.info>
2020-08-17 16:11:45 +1000

    Some changes for HPC

--- dataset_to_dbstruct.py
+++ dataset_to_dbstruct.py
@@ -5,7 +5,7 @@ from mapillary_sls.datasets.msls import MSLS
 from mapillary_sls.utils.eval import download_msls_sample
 
 import sys
-sys.path.append('/home/tobias/robot/NetVLAD2.0/pytorch-NetVlad-Nanne/')
+sys.path.append('../NetVLAD2.0/pytorch-NetVlad-Nanne/')
 from datasets import parse_db_struct, save_db_struct, dbStruct
 
 
@@ -13,10 +13,6 @@ def main():
 
     parser = argparse.ArgumentParser()
     root_default = Path(__file__).parent / 'MSLS_sample'
-    parser.add_argument('--prediction',
-                        type=Path,
-                        default=Path(__file__).parent / 'files' / 'example_msls_im2im_prediction.csv',
-                        help='Path to the prediction to be evaluated')
     parser.add_argument('--msls-root',
                         type=Path,
                         default=root_default,
@@ -35,19 +31,11 @@ def main():
                         default='im2im',
                         help='Task to evaluate on: '
                              '[im2im, seq2im, im2seq, seq2seq]')
-    parser.add_argument('--seq-length',
-                        type=int,
-                        default=3,
-                        help='Sequence length to evaluate on for seq2X and X2seq tasks')
     parser.add_argument('--subtask',
                         type=str,
                         default='all',
                         help='Subtask to evaluate on: '
                              '[all, s2w, w2s, o2n, n2o, d2n, n2d]')
-    parser.add_argument('--output',
-                        type=Path,
-                        default=None,
-                        help='Path to dump the metrics to')
     args = parser.parse_args()
 
     if not args.msls_root.exists():
@@ -59,9 +47,7 @@ def main():
 
     # select for which ks to evaluate
     ks = [1, 5, 10, 20]
-    if args.task == 'im2im' and args.seq_length > 1:
-        print(f"Ignoring sequence length {args.seq_length} for the im2im task. (Setting to 1)")
-        args.seq_length = 1
+    args.seq_length = 1
 
     dataset = MSLS(args.msls_root, cities = args.cities, mode = 'val', posDistThr = args.threshold, 
                     task = args.task, seq_length = args.seq_length, subtask = args.subtask)
@@ -91,7 +77,7 @@ def main():
     db = dbStruct(whichSet, dataset, dbImage, utmDb, qImage, utmQ, numDb, numQ, posDistThr,
                   posDistSqThr, nonTrivPosDistSqThr, dbTimeStamp, qTimeStamp, gpsDb, gpsQ)
 
-    save_db_struct('MSLS_sample/datasets/mapillary.mat', db)
+    save_db_struct('mapillary' + args.cities + '.mat', db)
 
 
 if __name__ == "__main__":
